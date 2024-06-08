20958450e87618d3e37565c16dd0e9f2f7b7ef07
Ek-04 <wkariuki.e@gmail.com>
2024-06-08 13:09:08 +0300

    commit1

---
+++ safety_application/lib/services/auth/home_or_getStarted.dart
@@ -0,0 +1,35 @@
+// ignore: file_names
+import 'package:flutter/material.dart';
+import 'package:safety_application/pages/Home_page.dart';
+import 'package:safety_application/services/auth/login_or_register.dart';
+
+class HomeOrGetstarted extends StatefulWidget {
+final VoidCallback? onTap;
+
+  const HomeOrGetstarted({super.key,  this.onTap});
+
+  @override
+  State<HomeOrGetstarted> createState() => _HomeOrGetstartedState();
+}
+
+class _HomeOrGetstartedState extends State<HomeOrGetstarted> {
+  bool showHomePage = true;
+
+  void togglepages() {
+    setState(() {
+      showHomePage = !showHomePage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showHomePage) {
+      return HomePage(
+        onTap: togglepages,
+        text: '',
+      );
+    } else {
+      return LoginOrRegister(onTap: togglepages);
+    }
+  }
+}
